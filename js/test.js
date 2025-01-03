dfd27099ca2f260bcf07daefcb05a5370fc805b1
Steve <steve@st2g.net>
2025-01-03 14:39:15 -0600

    Add button with click function

--- js/test.js
+++ js/test.js
@@ -4,3 +4,13 @@ function getTest(){
 }
 
 getTest();
+
+const testButton = document.getElementById("test-button");
+const resultsSpan = document.getElementById("test-span");
+function testToSpan(){
+    resultsSpan.innerHTML += "test</br>";
+}
+
+testButton.addEventListener('click', () => {
+    testToSpan();
+}
