fa4746c75273ca255f71e756216339f861d96181
Steve <steve@st2g.net>
2025-04-23 11:28:24 -0500

    Added Tic Tac Toe board

--- js/test.js
+++ js/test.js
@@ -7,10 +7,31 @@ getTest();
 
 const testButton = document.getElementById("test-button");
 const resultsSpan = document.getElementById("test-span");
+<<<<<<< HEAD
 function testToSpan() {
     resultsSpan.innerHTML += "test</br>";
+=======
+function testToSpan(){
+    resultsSpan.innerHTML += "</br>test";
+>>>>>>> 4ed589a (Added Tic Tac Toe board)
 }
 
 testButton.addEventListener('click', () => {
     testToSpan();
 });
+
+// Add function to generate a tic tac toe board on click
+const tictacButton = document.getElementById("tictac-button");
+const tictactoeBoard = document.getElementById("tictactoe-board-span");
+
+function createBoard(){
+    tictactoe-board-span.innerHTML = """
+   |   |   
+---+---+---
+   |   |   
+---+---+---
+   |   |   
+"""
+}
+
+tictacButton.addEventListener('click', createBoard());
